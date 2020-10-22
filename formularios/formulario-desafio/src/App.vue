610d07b434d2941d25878566aac39b94cb29fbb3
Carlos Moreira <carlosfwm@gmail.com>
2020-10-21 22:20:28 -0300

    formulário-desafio

--- formularios/formulario-desafio/src/App.vue
+++ formularios/formulario-desafio/src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<h1>Formulário Desafio</h1>
 		<div class="conteudo">
-			<form class="painel">
+			<form class="painel" v-if="!enviado">
 				<div class="cabecalho">Formulário</div>
 				<!-- Exercicio 01 -->
 				<!-- Criar uma formulário de registro -->
@@ -10,18 +10,42 @@
 				<!-- Email -->
 				<!-- Senha -->
 				<!-- Armazenar Dados? (Sim/Não) -->
-
+				<NomeCompleto v-model="nomeCompleto"/>
+				<Rotulo nome="Email">
+					<input type="text" v-model="email">
+				</Rotulo>
+				<Rotulo nome="Senha">
+					<input type="password" v-model="senha">
+				</Rotulo>
+				<Rotulo nome="Armazenar Dados?">
+					<input type="checkbox" v-model="armazenarDados">
+				</Rotulo>
 				<!-- Exercicio 02 -->
 				<!-- Só mostrar o fomulário de não tiver sido submetido -->
 				<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
-				
+				<hr>
+				<button @click.prevent="enviar">Enviar</button>
 				<!-- Exercicio 03 -->
 				<!-- Crie um componente personalizado NomeCompleto -->
 				<!-- Esse componente deve receber Nome e Sobrenome -->
 			</form>
-			<div class="painel">
+			<div class="painel" v-else>
 				<div class="cabecalho">Resultado</div>
-
+				<Rotulo nome="Nome">
+					<span>{{ nomeCompleto.nome }}</span>
+				</Rotulo>
+				<Rotulo nome="Sobrenome">
+					<span>{{ nomeCompleto.sobrenome }}</span>
+				</Rotulo>
+				<Rotulo nome="Email">
+					<span>{{ email }}</span>
+				</Rotulo>
+				<Rotulo nome="Senha">
+					<span>{{ senha }}</span>
+				</Rotulo>
+				<Rotulo nome="Armazenar Dados?">
+					<span>{{ armazenarDados }}</span>
+				</Rotulo>
 			</div>
 		</div>
 	</div>
@@ -29,10 +53,28 @@
 
 <script>
 import Rotulo from './components/Rotulo.vue'
+import NomeCompleto from './components/NomeCompleto.vue'
 
 export default {
 	name: 'app',
-	components: { Rotulo }
+	components: { Rotulo, NomeCompleto },
+	data() {
+		return {
+			enviado: false,
+			nomeCompleto: {
+				nome: '',
+				sobrenome: ''
+			},
+			email: '',
+			senha: '',
+			armazenarDados: true
+		}
+	},
+	methods: {
+		enviar() {
+			this.enviado = true
+		}
+	}
 }
 </script>
 
